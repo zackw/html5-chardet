43da394d973c02e071dbe59c27e8d50a94ee5192
Zack Weinberg <zackw@panix.com>
2015-10-15 11:23:12 -0400

    Eliminate use of variable-length arrays in prescan.c.
    Instead, we precalculate the minimum and maximum length of a label
    that *could* be in the table, bail out of validate_charset early if
    the label's length is out of range, and then we can use a fixed-length
    stack buffer.
    
    I am open to clever ideas for setting UNCANON_LABEL_MIN/MAX with
    preprocessor magic instead of an awk script.

--- Makefile
+++ Makefile
@@ -1,16 +1,22 @@
+AWK    = awk
 CC     = cc
 CFLAGS = -g -O2 -std=c11 -pedantic -Wall -Wextra
 
 all: test-prescan
 clean:
-	-rm -f test-prescan test-prescan.o prescan.o
+	-rm -f test-prescan test-prescan.o prescan.o \
+               uncanon-max.inc uncanon-max.incT
 check: all
 	./test-prescan tests/*.dat
 
 test-prescan: test-prescan.o prescan.o
 	$(CC) $(CFLAGS) test-prescan.o prescan.o -o test-prescan
 
-prescan.o: prescan.c prescan.h
+prescan.o: prescan.c prescan.h encodings.inc uncanon-max.inc
 test-prescan.o: test-prescan.c prescan.h
+
+uncanon-max.inc: encodings.inc uncanon-max.awk
+	$(AWK) -f uncanon-max.awk encodings.inc > uncanon-max.incT
+	mv -f uncanon-max.incT uncanon-max.inc
 
 .PHONY: all clean check
--- prescan.c
+++ prescan.c
@@ -43,16 +43,16 @@ static const char CONTENT_TYPE[] = "content-type";
 #define CONTENT_TYPE_LEN (sizeof CONTENT_TYPE - 1)
 
 /* Constants for the state machine that recognizes the tag name "meta". */
-#define NO 0
-#define M  1
-#define E  2
-#define T  3
-#define A  4
+enum {
+  NO = 0, M, E, T, A
+};
 
 /* Constants for handling http-equiv. */
-#define HTTP_EQUIV_NOT_SEEN     0
-#define HTTP_EQUIV_CONTENT_TYPE 1
-#define HTTP_EQUIV_OTHER        2
+enum {
+  HTTP_EQUIV_NOT_SEEN = 0,
+  HTTP_EQUIV_CONTENT_TYPE,
+  HTTP_EQUIV_OTHER
+};
 
 #define isAsciiWhitespace(c) \
   ((c) == ' ' || (c) == '\t' || (c) == '\r' || (c) == '\n' || (c) == '\f')
@@ -65,232 +65,16 @@ struct canonical_encoding
   const char *canonical;
 };
 
-/* This is the list of encodings that browsers are required to
-   recognize, from http://encoding.spec.whatwg.org/#encodings.
-   It has been sorted by field 0 to permit binary search, and the
-   substitution of "windows-1252" for "x-user-defined", mandated by
-   the prescan algorithm's step 2.14, has been applied to the table.
-   (The substitution of utf-8 for utf-16, mandated by step 2.13, is
-   handled below, because we *don't* want to do it in
-   canonical_encoding_for_label.)  */
+/* The canonical list of encodings ... */
 static const struct canonical_encoding canonical_encodings[] = {
-  { "866",                 "ibm866"         },
-  { "ansi_x3.4-1968",      "windows-1252"   },
-  { "arabic",              "iso-8859-6"     },
-  { "ascii",               "windows-1252"   },
-  { "asmo-708",            "iso-8859-6"     },
-  { "big5",                "big5"           },
-  { "big5-hkscs",          "big5"           },
-  { "chinese",             "gbk"            },
-  { "cn-big5",             "big5"           },
-  { "cp1250",              "windows-1250"   },
-  { "cp1251",              "windows-1251"   },
-  { "cp1252",              "windows-1252"   },
-  { "cp1253",              "windows-1253"   },
-  { "cp1254",              "windows-1254"   },
-  { "cp1255",              "windows-1255"   },
-  { "cp1256",              "windows-1256"   },
-  { "cp1257",              "windows-1257"   },
-  { "cp1258",              "windows-1258"   },
-  { "cp819",               "windows-1252"   },
-  { "cp866",               "ibm866"         },
-  { "csbig5",              "big5"           },
-  { "cseuckr",             "euc-kr"         },
-  { "cseucpkdfmtjapanese", "euc-jp"         },
-  { "csgb2312",            "gbk"            },
-  { "csibm866",            "ibm866"         },
-  { "csiso2022jp",         "iso-2022-jp"    },
-  { "csiso2022kr",         "replacement"    },
-  { "csiso58gb231280",     "gbk"            },
-  { "csiso88596e",         "iso-8859-6"     },
-  { "csiso88596i",         "iso-8859-6"     },
-  { "csiso88598e",         "iso-8859-8"     },
-  { "csiso88598i",         "iso-8859-8-i"   },
-  { "csisolatin1",         "windows-1252"   },
-  { "csisolatin2",         "iso-8859-2"     },
-  { "csisolatin3",         "iso-8859-3"     },
-  { "csisolatin4",         "iso-8859-4"     },
-  { "csisolatin5",         "windows-1254"   },
-  { "csisolatin6",         "iso-8859-10"    },
-  { "csisolatin9",         "iso-8859-15"    },
-  { "csisolatinarabic",    "iso-8859-6"     },
-  { "csisolatincyrillic",  "iso-8859-5"     },
-  { "csisolatingreek",     "iso-8859-7"     },
-  { "csisolatinhebrew",    "iso-8859-8"     },
-  { "cskoi8r",             "koi8-r"         },
-  { "csksc56011987",       "euc-kr"         },
-  { "csmacintosh",         "macintosh"      },
-  { "csshiftjis",          "shift_jis"      },
-  { "cyrillic",            "iso-8859-5"     },
-  { "dos-874",             "windows-874"    },
-  { "ecma-114",            "iso-8859-6"     },
-  { "ecma-118",            "iso-8859-7"     },
-  { "elot_928",            "iso-8859-7"     },
-  { "euc-jp",              "euc-jp"         },
-  { "euc-kr",              "euc-kr"         },
-  { "gb18030",             "gb18030"        },
-  { "gb2312",              "gbk"            },
-  { "gb_2312",             "gbk"            },
-  { "gb_2312-80",          "gbk"            },
-  { "gbk",                 "gbk"            },
-  { "greek",               "iso-8859-7"     },
-  { "greek8",              "iso-8859-7"     },
-  { "hebrew",              "iso-8859-8"     },
-  { "hz-gb-2312",          "replacement"    },
-  { "ibm819",              "windows-1252"   },
-  { "ibm866",              "ibm866"         },
-  { "iso-2022-cn",         "replacement"    },
-  { "iso-2022-cn-ext",     "replacement"    },
-  { "iso-2022-jp",         "iso-2022-jp"    },
-  { "iso-2022-kr",         "replacement"    },
-  { "iso-8859-1",          "windows-1252"   },
-  { "iso-8859-10",         "iso-8859-10"    },
-  { "iso-8859-11",         "windows-874"    },
-  { "iso-8859-13",         "iso-8859-13"    },
-  { "iso-8859-14",         "iso-8859-14"    },
-  { "iso-8859-15",         "iso-8859-15"    },
-  { "iso-8859-16",         "iso-8859-16"    },
-  { "iso-8859-2",          "iso-8859-2"     },
-  { "iso-8859-3",          "iso-8859-3"     },
-  { "iso-8859-4",          "iso-8859-4"     },
-  { "iso-8859-5",          "iso-8859-5"     },
-  { "iso-8859-6",          "iso-8859-6"     },
-  { "iso-8859-6-e",        "iso-8859-6"     },
-  { "iso-8859-6-i",        "iso-8859-6"     },
-  { "iso-8859-7",          "iso-8859-7"     },
-  { "iso-8859-8",          "iso-8859-8"     },
-  { "iso-8859-8-e",        "iso-8859-8"     },
-  { "iso-8859-8-i",        "iso-8859-8-i"   },
-  { "iso-8859-9",          "windows-1254"   },
-  { "iso-ir-100",          "windows-1252"   },
-  { "iso-ir-101",          "iso-8859-2"     },
-  { "iso-ir-109",          "iso-8859-3"     },
-  { "iso-ir-110",          "iso-8859-4"     },
-  { "iso-ir-126",          "iso-8859-7"     },
-  { "iso-ir-127",          "iso-8859-6"     },
-  { "iso-ir-138",          "iso-8859-8"     },
-  { "iso-ir-144",          "iso-8859-5"     },
-  { "iso-ir-148",          "windows-1254"   },
-  { "iso-ir-149",          "euc-kr"         },
-  { "iso-ir-157",          "iso-8859-10"    },
-  { "iso-ir-58",           "gbk"            },
-  { "iso8859-1",           "windows-1252"   },
-  { "iso8859-10",          "iso-8859-10"    },
-  { "iso8859-11",          "windows-874"    },
-  { "iso8859-13",          "iso-8859-13"    },
-  { "iso8859-14",          "iso-8859-14"    },
-  { "iso8859-15",          "iso-8859-15"    },
-  { "iso8859-2",           "iso-8859-2"     },
-  { "iso8859-3",           "iso-8859-3"     },
-  { "iso8859-4",           "iso-8859-4"     },
-  { "iso8859-5",           "iso-8859-5"     },
-  { "iso8859-6",           "iso-8859-6"     },
-  { "iso8859-7",           "iso-8859-7"     },
-  { "iso8859-8",           "iso-8859-8"     },
-  { "iso8859-9",           "windows-1254"   },
-  { "iso88591",            "windows-1252"   },
-  { "iso885910",           "iso-8859-10"    },
-  { "iso885911",           "windows-874"    },
-  { "iso885913",           "iso-8859-13"    },
-  { "iso885914",           "iso-8859-14"    },
-  { "iso885915",           "iso-8859-15"    },
-  { "iso88592",            "iso-8859-2"     },
-  { "iso88593",            "iso-8859-3"     },
-  { "iso88594",            "iso-8859-4"     },
-  { "iso88595",            "iso-8859-5"     },
-  { "iso88596",            "iso-8859-6"     },
-  { "iso88597",            "iso-8859-7"     },
-  { "iso88598",            "iso-8859-8"     },
-  { "iso88599",            "windows-1254"   },
-  { "iso_8859-1",          "windows-1252"   },
-  { "iso_8859-15",         "iso-8859-15"    },
-  { "iso_8859-1:1987",     "windows-1252"   },
-  { "iso_8859-2",          "iso-8859-2"     },
-  { "iso_8859-2:1987",     "iso-8859-2"     },
-  { "iso_8859-3",          "iso-8859-3"     },
-  { "iso_8859-3:1988",     "iso-8859-3"     },
-  { "iso_8859-4",          "iso-8859-4"     },
-  { "iso_8859-4:1988",     "iso-8859-4"     },
-  { "iso_8859-5",          "iso-8859-5"     },
-  { "iso_8859-5:1988",     "iso-8859-5"     },
-  { "iso_8859-6",          "iso-8859-6"     },
-  { "iso_8859-6:1987",     "iso-8859-6"     },
-  { "iso_8859-7",          "iso-8859-7"     },
-  { "iso_8859-7:1987",     "iso-8859-7"     },
-  { "iso_8859-8",          "iso-8859-8"     },
-  { "iso_8859-8:1988",     "iso-8859-8"     },
-  { "iso_8859-9",          "windows-1254"   },
-  { "iso_8859-9:1989",     "windows-1254"   },
-  { "koi",                 "koi8-r"         },
-  { "koi8",                "koi8-r"         },
-  { "koi8-r",              "koi8-r"         },
-  { "koi8-u",              "koi8-u"         },
-  { "koi8_r",              "koi8-r"         },
-  { "korean",              "euc-kr"         },
-  { "ks_c_5601-1987",      "euc-kr"         },
-  { "ks_c_5601-1989",      "euc-kr"         },
-  { "ksc5601",             "euc-kr"         },
-  { "ksc_5601",            "euc-kr"         },
-  { "l1",                  "windows-1252"   },
-  { "l2",                  "iso-8859-2"     },
-  { "l3",                  "iso-8859-3"     },
-  { "l4",                  "iso-8859-4"     },
-  { "l5",                  "windows-1254"   },
-  { "l6",                  "iso-8859-10"    },
-  { "l9",                  "iso-8859-15"    },
-  { "latin1",              "windows-1252"   },
-  { "latin2",              "iso-8859-2"     },
-  { "latin3",              "iso-8859-3"     },
-  { "latin4",              "iso-8859-4"     },
-  { "latin5",              "windows-1254"   },
-  { "latin6",              "iso-8859-10"    },
-  { "logical",             "iso-8859-8-i"   },
-  { "mac",                 "macintosh"      },
-  { "macintosh",           "macintosh"      },
-  { "ms_kanji",            "shift_jis"      },
-  { "shift-jis",           "shift_jis"      },
-  { "shift_jis",           "shift_jis"      },
-  { "sjis",                "shift_jis"      },
-  { "sun_eu_greek",        "iso-8859-7"     },
-  { "tis-620",             "windows-874"    },
-  { "unicode-1-1-utf-8",   "utf-8"          },
-  { "us-ascii",            "windows-1252"   },
-  { "utf-16",              "utf-16le"       },
-  { "utf-16be",            "utf-16be"       },
-  { "utf-16le",            "utf-16le"       },
-  { "utf-8",               "utf-8"          },
-  { "utf8",                "utf-8"          },
-  { "visual",              "iso-8859-8"     },
-  { "windows-1250",        "windows-1250"   },
-  { "windows-1251",        "windows-1251"   },
-  { "windows-1252",        "windows-1252"   },
-  { "windows-1253",        "windows-1253"   },
-  { "windows-1254",        "windows-1254"   },
-  { "windows-1255",        "windows-1255"   },
-  { "windows-1256",        "windows-1256"   },
-  { "windows-1257",        "windows-1257"   },
-  { "windows-1258",        "windows-1258"   },
-  { "windows-31j",         "shift_jis"      },
-  { "windows-874",         "windows-874"    },
-  { "windows-949",         "euc-kr"         },
-  { "x-cp1250",            "windows-1250"   },
-  { "x-cp1251",            "windows-1251"   },
-  { "x-cp1252",            "windows-1252"   },
-  { "x-cp1253",            "windows-1253"   },
-  { "x-cp1254",            "windows-1254"   },
-  { "x-cp1255",            "windows-1255"   },
-  { "x-cp1256",            "windows-1256"   },
-  { "x-cp1257",            "windows-1257"   },
-  { "x-cp1258",            "windows-1258"   },
-  { "x-euc-jp",            "euc-jp"         },
-  { "x-gbk",               "gbk"            },
-  { "x-mac-cyrillic",      "x-mac-cyrillic" },
-  { "x-mac-roman",         "macintosh"      },
-  { "x-mac-ukrainian",     "x-mac-cyrillic" },
-  { "x-sjis",              "shift_jis"      },
-  { "x-user-defined",      "windows-1252"   },
-  { "x-x-big5",            "big5"           },
+#define E(l,e) { l, e },
+#include "encodings.inc"
+#undef E
 };
+
+/* ... and the minimum and maximum number of bytes required to hold a
+   not-yet-canonicalized encoding label.  */
+#include "uncanon-max.inc"
 
 static int
 canonical_encoding_compar(const void *k, const void *v)
@@ -325,7 +109,12 @@ validate_charset(const char *charset, size_t charset_len)
     charset_len--;
   }
 
-  char label[charset_len + 1];
+  if (charset_len < UNCANON_LABEL_MIN || charset_len > UNCANON_LABEL_MAX)
+    /* The label is too short or too long, it cannot possibly be in
+       the table. */
+    return 0;
+
+  char label[UNCANON_LABEL_MAX + 1];
   for (size_t i = 0; i < charset_len; i++) {
     label[i] = toAsciiLowerCase(charset[i]);
   }
